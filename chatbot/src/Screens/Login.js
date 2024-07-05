5c698ddd854a539f474cbf6112ae8bb7c0e3624f
Rakshitha365 <reddyrakshitha857@gmail.com>
2024-07-05 20:54:45 +0530

    Added Login Page

--- chatbot/src/Screens/Login.js
+++ chatbot/src/Screens/Login.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import "../CSS/Login.css";
 
-export default function Login() {
+function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label>Email:</label>
+          <input type="email" required />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input type="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
+
+export default Login;
